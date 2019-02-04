dc78950f250b9eb2d5200e8a21add9eae4493895
Jason Axelson <jason.axelson@gmail.com>
2019-02-03 23:49:55 -1000

    Add the parser harness and some supporting files

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule LsProxy.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      escript: [main_module: LsProxy.CLI],
+      elixirc_paths: elixirc_paths(Mix.env)
     ]
   end
 
@@ -18,9 +20,14 @@ defmodule LsProxy.MixProject do
     ]
   end
 
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:jason, "~> 1.1"},
+      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
