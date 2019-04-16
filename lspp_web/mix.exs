43e9c676834d25b2cb453184040c20f24df8da21
Jason Axelson <jason.axelson@gmail.com>
2019-04-16 08:24:26 -1000

    Switch dependency so lspp_web depends on ls_proxy

--- lspp_web/mix.exs
+++ lspp_web/mix.exs
@@ -38,7 +38,8 @@ defmodule LsppWeb.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:ls_proxy, path: "../ls_proxy"}
     ]
   end
 end
