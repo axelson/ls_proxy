2e2191aabf27272ffdac607f8bcd652eafb0ddf4
Jason Axelson <jason.axelson@gmail.com>
2019-04-27 11:53:36 -1000

    Lower the debugging

--- app/config/prod.exs
+++ app/config/prod.exs
@@ -16,8 +16,7 @@ config :lspp_web, LsppWebWeb.Endpoint,
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
-# Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :warn
 
 # ## SSL Support
 #
