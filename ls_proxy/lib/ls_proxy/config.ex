8271a87aa5758f6401eb3b04fddac6485050668f
Jason Axelson <jason.axelson@gmail.com>
2020-09-02 21:59:30 -1000

    More crossplatform elixir-ls location finding

--- ls_proxy/lib/ls_proxy/config.ex
+++ ls_proxy/lib/ls_proxy/config.ex
@@ -2,13 +2,14 @@ defmodule LsProxy.Config do
   def language_server_script_path do
     case Application.fetch_env!(:ls_proxy, :proxy_to) do
       "elixir_ls" ->
+        # In-repo ElixirLS
         Path.join(__DIR__, "../../../elixir-ls/release/language_server.sh") |> Path.expand()
 
       "elixir_ls_dev" ->
-        Path.expand("~/dev/forks/elixir-ls/release/language_server.sh")
+        Path.join(System.user_home!(), "dev/forks/elixir-ls/release/language_server.sh") |> Path.expand()
 
       "ls_proxy" ->
-        "/home/jason/dev/ls_proxy/ls_proxy"
+        Path.join(System.user_home!(), "dev/ls_proxy/ls_proxy") |> Path.expand()
     end
   end
 end
